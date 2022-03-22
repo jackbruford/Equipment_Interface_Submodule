e509c740e093879b546500c641a48632f44a7ebc
Jack Bruford <jack.bruford@eng.ox.ac.uk>
2022-03-22 12:02:44 +0000

    working MS054 python code

--- TekScope/MSO54Comms.py
+++ TekScope/MSO54Comms.py
@@ -3,6 +3,7 @@
 
 import pyvisa
 import numpy as np
+from struct import unpack
 
 class MSO54:
     def __init__(self):
@@ -11,15 +12,14 @@ class MSO54:
         self.BYTE_NR = 2     # Number of bytes per waveform point - NOTE: Must change binblockread() precision if this value
         # is changed!
         self.RESOURCE_STRING = 'USB::0x0699::0x0522::C012598::INSTR' #Use Tek's VISA Resource Manager to find the resource string
-        self.CHAN = ['CH1','CH2','CH3','CH4']  #Oscilloscope channels to use - all channels listed must be enabled on scope first otherwise might crash
-        self.wave = []  #output waveform data from scope
+        self.CHAN = []  #Oscilloscope channels to use - all channels listed must be enabled on scope first otherwise might crash
+        self.wave = {}  #output waveform data from scope
         self.status = []
         self.rm = pyvisa.ResourceManager()
 
     def open(self):
         if hasattr(self, 'inst'):
-            print('MSO54 already open. Exiting without doing anything.')
-            raise  # device already exists
+            raise ConnectionError('MSO54 already open. Exiting without doing anything.')
 
         self.inst = self.rm.open_resource(self.RESOURCE_STRING)
         self.inst.read_termination ="\n"
@@ -37,12 +37,18 @@ class MSO54:
         print('Scope open completed\n')
         print(self.inst.query('*IDN?;'))
 
+    def set_channels(self):
+        if not hasattr(self, 'inst'):
+            raise ConnectionError('MSO54 not opened')
+        resp = self.inst.query("DATa:SOUrce:AVAILable?")
+        self.CHAN = [str(item) for item in resp.split(",")]
+
     def close(self):
         if not hasattr(self, 'inst'):
-            print('MSO54 not open. Cannot close')
-            raise  # device already exists
+            raise ConnectionError('MSO54 not open. Cannot close')
         else:
             self.inst.close()
+            delattr(self, 'inst')
 
 
     def set(self):
@@ -55,15 +61,31 @@ class MSO54:
         self.inst.write('TRIG:A:MODE NORMAL')
         self.inst.write('ACQuire:StopAfter Sequence')
     def arm(self):
+        if not hasattr(self, 'inst'):
+            raise ConnectionError('MSO54 not opened')
         self.inst.write('ACQuire:State 1')
 
+    def set_horizontal_scale(self, T_per_division):
+        """
+        Method for adjusting the horizontal scale, T_per_division is in seconds
+        """
+        if not hasattr(self, 'inst'):
+            raise ConnectionError('MSO54 not opened')
+        self.inst.write("HORIZONTAL:MODE:SCALE " + str(T_per_division))
+
+
     def read(self):
-        wave = {}
+        if not hasattr(self, 'inst'):
+            raise ConnectionError('MSO54 not opened')
+        # turn off header
+        self.inst.write("HEADER OFF")
+        self.set_channels()
+
         for i, ch in enumerate(self.CHAN): #for each channel
             # Specify waveform source
             self.inst.write(':Data:Source ' + ch)
             # Specify waveform data format - most sig. byte transferred first
-            self.inst.write(':Data:Encdg SRIBinary')
+            self.inst.write(':Data:Encdg ASCI') # charged from SRIbinary, alternative RPB
             # Number of bits per waveform point
             self.inst.write('WFMOutPre:BIT_Nr '+ str(self.BIT_NR))
             # Number of bytes per data point
@@ -71,17 +93,23 @@ class MSO54:
             # Specify that we want to transfer N_SAMP points
             self.inst.write(':Data:Start 1')
             self.inst.write(':Data:Stop '+str(self.N_SAMP))
+
             #Get scale and offset
-            verticalScale  = self.inst.query('WFMOUTPre:YMULT?')
-            yOffset = self.inst.query( 'WFMOutpre:YOFF?')
+            verticalScale  = float(self.inst.query('WFMOUTPre:YMULT?'))
+            yOffset = float(self.inst.query( 'WFMOutpre:YOFF?'))
+            yzero = float(self.inst.query('WFMPRE:YZERO?'))
+
             #Get the sample interval in seconds
-            Ts = self.inst.query('WFMOutPre:XINcr?')
+            Ts = float(self.inst.query('WFMOutPre:XINcr?'))
+
+
             # Request the waveform data
-            rawdata = self.inst.query('CURVE?')
-            curve = np.array(list(map(float, rawdata.split(","))))
+            ADC_wave = self.inst.query_ascii_values('CURVE?', container= np.array)
+
             # Read in the data from the buffer, and scale
-            wave[ch] = list(float(verticalScale) * curve + float(yOffset))
+            self.wave[ch] = list((ADC_wave - yOffset) * verticalScale + yzero)
+
 
         # Get time series
-        wave['t'] = Ts*np.arange(1,self.N_SAMP)
-
+        self.wave['t'] = Ts*np.arange(1, self.N_SAMP)
+        return self.wave
