12f78b906e18dcf2a0acc6b888d58e361b80b234
Jack Bruford <jack.bruford@eng.ox.ac.uk>
2022-03-30 13:41:23 +0100

    Update some scope parameters

--- TekScope/MSO54Comms.py
+++ TekScope/MSO54Comms.py
@@ -5,15 +5,16 @@ import pyvisa
 import numpy as np
 from struct import unpack
 
+
 class MSO54:
     def __init__(self):
-        self.N_SAMP = 1250   # number of samplestocapture
-        self.BIT_NR = 12     # Numberofbitsperwaveformpoint
-        self.BYTE_NR = 2     # Number of bytes per waveform point - NOTE: Must change binblockread() precision if this value
+        self.N_SAMP = 1250  # number of samplestocapture
+        self.BIT_NR = 12  # Numberofbitsperwaveformpoint
+        self.BYTE_NR = 2  # Number of bytes per waveform point - NOTE: Must change binblockread() precision if this value
         # is changed!
-        self.RESOURCE_STRING = 'USB::0x0699::0x0522::C012598::INSTR' #Use Tek's VISA Resource Manager to find the resource string
-        self.CHAN = []  #Oscilloscope channels to use - all channels listed must be enabled on scope first otherwise might crash
-        self.wave = {}  #output waveform data from scope
+        self.RESOURCE_STRING = 'USB::0x0699::0x0522::C012598::INSTR'  # Use Tek's VISA Resource Manager to find the resource string
+        self.CHAN = []  # Oscilloscope channels to use - all channels listed must be enabled on scope first otherwise might crash
+        self.wave = {}  # output waveform data from scope
         self.status = []
         self.rm = pyvisa.ResourceManager()
 
@@ -22,8 +23,9 @@ class MSO54:
             raise ConnectionError('MSO54 already open. Exiting without doing anything.')
 
         self.inst = self.rm.open_resource(self.RESOURCE_STRING)
-        self.inst.read_termination ="\n"
-        self.inst.write_termination = "\n"
+        # On some PC this doesnt work if the following two lines are uncommented
+        # self.inst.read_termination = '\n'
+        # self.inst.write_termination = '\n'
         self.inst.timeout = 1000
         try:
             print("Opened connection with ", self.inst.query('*IDN?;'))
@@ -31,11 +33,9 @@ class MSO54:
             print('Error occurred while trying to open connection with multimeter.\nError:\n', e)
         self.inst.timeout = 1000
 
-
         self.InputBufferSize = 65536
 
         print('Scope open completed\n')
-        print(self.inst.query('*IDN?;'))
 
     def set_channels(self):
         if not hasattr(self, 'inst'):
@@ -50,9 +50,8 @@ class MSO54:
             self.inst.close()
             delattr(self, 'inst')
 
-
     def set(self):
-        #Currently assumes most setup is done through scope screen interface
+        # Currently assumes most setup is done through scope screen interface
         # Setup trigger - complicated - best done on screen for now
         self.inst.write('TRIG:A:TYPE EDGE')
         self.inst.write('TRIG:A:EDGE:SOURCE CH1')
@@ -60,6 +59,7 @@ class MSO54:
         self.inst.write('TRIG:A:LEVEL:CH1 10')
         self.inst.write('TRIG:A:MODE NORMAL')
         self.inst.write('ACQuire:StopAfter Sequence')
+
     def arm(self):
         if not hasattr(self, 'inst'):
             raise ConnectionError('MSO54 not opened')
@@ -76,7 +76,7 @@ class MSO54:
     def set_vertical_scale(self, channel, units_per_division):
         if not hasattr(self, 'inst'):
             raise ConnectionError('MSO54 not opened')
-        self.inst.write("CH"+ str(channel) + ":SCALE " + str(units_per_division).format("e"))
+        self.inst.write("CH" + str(channel) + ":SCALE " + str(units_per_division).format("e"))
 
     def set_vertical_offset(self, channel, offset):
         """
@@ -87,9 +87,9 @@ class MSO54:
         """
         if not hasattr(self, 'inst'):
             raise ConnectionError('MSO54 not opened')
-        self.inst.write("CH"+ str(channel) + ":SCALE " + str(offset).format("e"))
+        self.inst.write("CH" + str(channel) + ":SCALE " + str(offset).format("e"))
 
-    def set_vertical
+    # def set_vertical
 
     def read(self):
         if not hasattr(self, 'inst'):
@@ -98,35 +98,33 @@ class MSO54:
         self.inst.write("HEADER OFF")
         self.set_channels()
 
-        for i, ch in enumerate(self.CHAN): #for each channel
+        for i, ch in enumerate(self.CHAN):  # for each channel
             # Specify waveform source
             self.inst.write(':Data:Source ' + ch)
             # Specify waveform data format - most sig. byte transferred first
-            self.inst.write(':Data:Encdg ASCI') # charged from SRIbinary, alternative RPB
+            self.inst.write(':Data:Encdg ASCI')  # charged from SRIbinary, alternative RPB
             # Number of bits per waveform point
-            self.inst.write('WFMOutPre:BIT_Nr '+ str(self.BIT_NR))
+            self.inst.write('WFMOutPre:BIT_Nr ' + str(self.BIT_NR))
             # Number of bytes per data point
-            self.inst.write('Data:Width '+ str(self.BYTE_NR))
+            self.inst.write('Data:Width ' + str(self.BYTE_NR))
             # Specify that we want to transfer N_SAMP points
             self.inst.write(':Data:Start 1')
-            self.inst.write(':Data:Stop '+str(self.N_SAMP))
+            self.inst.write(':Data:Stop ' + str(self.N_SAMP))
 
-            #Get scale and offset
-            verticalScale  = float(self.inst.query('WFMOUTPre:YMULT?'))
-            yOffset = float(self.inst.query( 'WFMOutpre:YOFF?'))
+            # Get scale and offset
+            verticalScale = float(self.inst.query('WFMOUTPre:YMULT?'))
+            yOffset = float(self.inst.query('WFMOutpre:YOFF?'))
             yzero = float(self.inst.query('WFMPRE:YZERO?'))
 
-            #Get the sample interval in seconds
+            # Get the sample interval in seconds
             Ts = float(self.inst.query('WFMOutPre:XINcr?'))
 
-
             # Request the waveform data
-            ADC_wave = self.inst.query_ascii_values('CURVE?', container= np.array)
+            ADC_wave = self.inst.query_ascii_values('CURVE?', container=np.array)
 
             # Read in the data from the buffer, and scale
             self.wave[ch] = list((ADC_wave - yOffset) * verticalScale + yzero)
 
-
         # Get time series
-        self.wave['t'] = Ts*np.arange(1, self.N_SAMP)
+        self.wave['t'] = Ts * np.arange(1, self.N_SAMP)
         return self.wave
