0aaeecc7408aa980ef6b3737eb926a80b9f37898
Jack Bruford <jack.bruford@eng.ox.ac.uk>
2023-04-17 11:42:06 +0100

    Update keysight module

--- Keysight/Keysight.py
+++ Keysight/Keysight.py
@@ -17,6 +17,7 @@ class KeysightScope:
         self.CHAN = []  # Oscilloscope channels to use - all channels listed must be enabled on scope first otherwise might crash
         self.wave = {}  # output waveform data from scope
         self.status = []
+
         self.rm = pyvisa.ResourceManager()
 
     def open(self):
@@ -24,26 +25,31 @@ class KeysightScope:
             raise ConnectionError('DSOX2024A already open. Exiting without doing anything.')
 
         self.inst = self.rm.open_resource(self.RESOURCE_STRING)
-        self.inst.read_termination = "\n"
-        self.inst.write_termination = "\n"
+        self.inst.read_termination = '\n'
+        self.inst.write_termination = '\n'
         self.inst.timeout = 10000
+
+        # self.inst.set_visa_attribute(pyvisa.constants.VI_ATTR_ASRL_BAUD, 9600)
+        # self.inst.set_visa_attribute(pyvisa.constants.VI_ATTR_ASRL_DATA_BITS, 8)
+        # self.inst.set_visa_attribute(pyvisa.constants.VI_ATTR_ASRL_PARITY, pyvisa.constants.VI_ASRL_PAR_NONE)
+        # self.inst.set_visa_attribute(pyvisa.constants.VI_ATTR_ASRL_FLOW_CNTRL, pyvisa.constants.VI_ASRL_FLOW_DTR_DSR)
         try:
-            print("Opened connection with ", self.inst.query('*IDN?;'))
+            print("Opened connection with ", self.query('*IDN?;'))
         except Exception as e:
             print('Error occurred while trying to open connection with keysight oscilloscope.\nError:\n', e)
-        self.inst.timeout = 100
 
         self.InputBufferSize = 65536
 
         print('Scope open completed\n')
-        print(self.inst.query('*IDN?;'))
+        print(self.query('*IDN?;'))
+        # self.inst.write('*rst')
 
     def set_channels(self):  # not updated
         if not hasattr(self, 'inst'):
             raise ConnectionError('DSOX2024A not opened')
         displayed = [False, False, False, False]
         for channel in range(1, 5):
-            resp = self.inst.query(":CHAN" + str(channel) + ":DISPLAY?")
+            resp = self.query(":CHAN" + str(channel) + ":DISPLAY?")
             if resp == "1":
                 displayed[channel - 1] = True
             elif resp == "0":
@@ -59,13 +65,13 @@ class KeysightScope:
             self.inst.close()
             delattr(self, 'inst')
 
-    def set(self):
+    def set(self, trigger_voltage=-1):
         # Currently assumes most setup is done through scope screen interface
         # Setup trigger - complicated - best done on screen for now
         self.inst.write('TRIG:MODE EDGE')
         self.inst.write(':TRIG:EDGE:SOUR CHAN1')
         self.inst.write('TRIG:EDGE:SLOPE NEG')
-        self.inst.write('TRIG:EDGE:LEVEL 10')
+        self.inst.write('TRIG:EDGE:LEVEL '+str(trigger_voltage))
         self.inst.write('TRIG:SWEEP NORMAL')
 
         # self.inst.write('ACQuire:StopAfter Sequence')
@@ -79,21 +85,42 @@ class KeysightScope:
             raise ConnectionError('MSO54 not opened')
         self.inst.write(':RUN')
 
-    def arm(self):
+    def arm(self,HRes):
         if not hasattr(self, 'inst'):
             raise ConnectionError('DSOX2024A not opened')
-        self.inst.write(':SINGLE')
-        self.inst.write('ACQUIRE:TYPE NORMAL')
-        self.inst.write('ACQUIRE:COMPLETE 100')
-        print(self.CHAN)
-        self.inst.write('ACQUIRE:DIGI 1')
-        self.inst.write('ACQUIRE:POINTS AUTO')
 
-        msg = 'DIGITIZE:'
-        for i, ch in enumerate(self.CHAN):  # for each channel
-            msg += 'CHAN' + str(i) + ','
-        msg = msg[:-1]
-        self.inst.write(msg)
+        self.inst.write(':STOP')
+        opc = int(self.query('*OPC?'))
+        if HRes:
+            self.inst.write('ACQUIRE:TYPE HRESolution')
+        else:
+            self.inst.write('ACQUIRE:TYPE NORMAL')
+        # self.inst.write('ACQUIRE:COMPLETE 100')
+        # self.inst.write('ACQUIRE:DIGI 1')
+        # self.inst.write('ACQUIRE:POINTS AUTO')
+
+        # ese = self.query('*ESE?')
+        # self.inst.write('*ESE 1')
+
+        self.inst.write('*CLS')
+        self.inst.write(':SINGLE')
+        time.sleep(0.1)
+
+        # msg = 'DIGITIZE:'
+        # for i, ch in enumerate(self.CHAN):  # for each channel
+        #     msg += 'CHAN' + str(i) + ','
+        # msg = msg[:-1]
+        # self.inst.write(msg)
+        # self.inst.write('*OPC')
+        # stb = 0
+        # while stb ==0:
+        #     time.sleep(0.1)
+        #     stb = int(self.query('*STB?'))
+        # self.query('*ESR?')
+        # self.inst.write('*ESE '+ese)
+
+
+
 
     def set_horizontal_scale(self, T_per_division):
         """
@@ -108,11 +135,38 @@ class KeysightScope:
             "TIMEBASE:REF LEFT")  # sets the time reference to one division from the left side of the screen,
         # to the center of the screen, or to one division from the right side of the screen. {LEFT | CENT | RIGH}
 
+    def set_horizontal_position(self, position):
+        """
+        This command sets or queries the horizontal position as a percent of screen
+        width. When Horizontal Delay Mode is turned off, this command is equivalent
+        to adjusting the HORIZONTAL POSITION knob on the front panel. When
+        Horizontal Delay Mode is turned on, the horizontal position is forced to 50%
+
+        position: float - position as a percentage of the screen width
+        """
+        self.inst.write("HORIZONTAL:POSITION " + str(position))
+
     def set_vertical_scale(self, channel, units_per_division):
         n_divisions = 8
         if not hasattr(self, 'inst'):
             raise ConnectionError('DSOX2024A not opened')
         self.inst.write("CHAN" + str(channel) + ":RANGE " + str(units_per_division * n_divisions).format("e") + " V")
+
+    def set_vertical_offset(self, channel, offset):
+        """
+        This command sets the vertical offset for the specified analog channel in divisions
+        :param channel: channel number
+        :param offset: offset for the channel
+        :return: None
+        """
+        pass # probe offset cannot be set on this scope
+        # n_divisions = 8
+        # if not hasattr(self, 'inst'):
+        #     raise ConnectionError('MSO54 not opened')
+        # time.sleep(0.25)
+        # range= float(self.query(":CHAN"+str(channel)+":RANGE?"))
+        # offset_units = offset*range/n_divisions
+        # # self.inst.write(":CHAN" + str(channel) + ":OFFSET " + str(offset_units).format("e"))
 
     def set_vertical_position(self, channel, offset):
         """
@@ -128,13 +182,13 @@ class KeysightScope:
     def get_vertical_position(self, ch: int):
         if not hasattr(self, 'inst'):
             raise ConnectionError('MSO54 not opened')
-        offset = self.inst.query("CHAN" + str(ch) + ":OFFSET?")
+        offset = self.query("CHAN" + str(ch) + ":OFFSET?")
         try:
             offset_float = float(offset)
         except ValueError as e:
             raise e
             # print("Scope returned something that couldnt be processed into a float")
-            # offset = self.inst.query("CH" + str(ch) + ":OFFSET?")
+            # offset = self.query("CH" + str(ch) + ":OFFSET?")
             # offset_float = float(offset)
 
         return offset_float
@@ -145,7 +199,7 @@ class KeysightScope:
         width.
         :return float - position as a percentage of the screen width
         """
-        msg = self.inst.query("TIMEBASE:REF?")
+        msg = self.query("TIMEBASE:REF?")
         if msg == 'LEFT':
             return float(10)
         elif msg == 'CENT':
@@ -162,14 +216,15 @@ class KeysightScope:
     def set_channel_alt_units(self, channel: int, unit: str):
         if channel > 4 or channel < 1:
             raise ValueError("Invalid channel number")
-
-        if str.upper(unit) == "VOLT" or str.upper(unit) == "V":
-            self.inst.write("CHAN" + str(channel) + ":UNITS " + unit)
-        elif str.upper(unit) == "AMP" or str.upper(unit) == "A":
-            self.inst.write("CHAN" + str(channel) + ":UNITS " + unit)
+        if unit is not None:
+            if str.upper(unit) == "VOLT" or str.upper(unit) == "V":
+                self.inst.write("CHAN" + str(channel) + ":UNITS " + unit)
+            elif str.upper(unit) == "AMP" or str.upper(unit) == "A":
+                self.inst.write("CHAN" + str(channel) + ":UNITS " + unit)
+            else:
+                raise ValueError("Invalid unit parameter must be: 'AMP', 'A', 'VOLT' or 'V'")
         else:
-            raise ValueError("Invalid unit parameter must be: 'AMP', 'A', 'VOLT' or 'V'")
-
+            unit = "V"
     def set_channel_label(self, channel: int, label: str):
         if channel > 4 or channel < 1:
             raise ValueError("Invalid channel number")
@@ -188,7 +243,7 @@ class KeysightScope:
     def get_channel_terminationn(self, channel: int):
         if channel > 4 or channel < 1:
             raise ValueError("Invalid channel number")
-        msg = self.inst.query("CHAN" + str(channel) + ":IMPEDANCE?")
+        msg = self.query("CHAN" + str(channel) + ":IMPEDANCE?")
         if msg == "ONEM":
             return float(1e6)
         elif msg == "FIFT":
@@ -245,7 +300,7 @@ class KeysightScope:
         :return: samplerate in samples per second
         """
         n_divisions = 10
-        return float(self.inst.query("TIME:RANGE?") / n_divisions)
+        return float(self.query(':TIMEBASE:RANGE?')) / n_divisions
 
     def get_n_divisons(self):
         """
@@ -263,14 +318,26 @@ class KeysightScope:
         self.inst.write("MARKER:X2:DISPLAY 0")
         self.inst.write(":MARKER:X1Y1SOURCE CHANNEL" + str(channel))
         self.inst.write(":MARKER:X1Position " + str(position))
+        try:
+            self.query('*OPC?')
+            time.sleep(0.2) # needed to allow a subsequent trigger to occur after setting up cursor
+            # v_offset = self.get_vertical_offset(channel)
+            v_position = self.query(":MARKer:Y1Position?")
+        except pyvisa.VisaIOError:
+            self.query('*OPC?')
+            time.sleep(0.2)  # needed to allow a subsequent trigger to occur after setting up cursor
+            # v_offset = self.get_vertical_offset(channel)
+            v_position = self.query(":MARKer:Y1Position?")
 
-        # v_offset = self.get_vertical_offset(channel)
-        v_position = self.inst.query(":MARKer:Y1Position?")
 
         self.inst.write("MARKER:X1:DISPLAY 0")
+
+        if position > 1e36:
+            time.sleep(0.2)
+            v_position = self.query(":MARKer:Y1Position?")
+            if position > 1e36:
+                raise ValueError("Measurement returning invalid data, measuement = " + str(position))
         position = float(v_position)  # - v_offset
-        if position > 99e36:
-            raise ValueError("Measurement returning invalid data, measuement = " + str(position))
         return position
 
     def get_samplerate(self):
@@ -292,7 +359,7 @@ class KeysightScope:
         n_samples = int(n_divisions * t_division * samplerate)
         return n_samples
 
-    def read(self):
+    def read(self, n_samples=None):
         timeout = 1
         if not hasattr(self, 'inst'):
             raise ConnectionError('DSOX2024A not opened')
@@ -300,50 +367,106 @@ class KeysightScope:
 
         timestart = time.time()
         resp = 0
-        while int(resp) != 1 and time.time() - timestart < timeout:
-            resp = self.inst.query(":TER?")  # todo this line does not seem to correctly check for triggering
+        # while int(resp) != 1 and time.time() - timestart < timeout:
+        #     time.sleep(0.05)
+        #     resp = self.query(":TER?")
+        #     self.query('*OPC?')
+        # if int(resp) != 1:
+        #     raise TimeoutError("Acquire did not complete")#
+        RUN_BIT = 3
+        RUN_MASK = 1 << RUN_BIT
+        ACQ_DONE = 0
+        ACQ_NOT_DONE = 1 << RUN_BIT
+        stb = 8
+        Acq_State = ACQ_NOT_DONE
 
-        if int(resp) != 1:
-            raise TimeoutError("Aquire did not complete")
+        time.sleep(0.5)
+        while Acq_State == ACQ_NOT_DONE:
+            Status = int(self.query(':OPER:COND?'))
+            Acq_State = (Status & RUN_MASK)
+            if Acq_State == ACQ_DONE:
+                break
+            print(Status)
+            time.sleep(0.1)
+        if Acq_State == ACQ_DONE:  # Acquisition fully completed
+            print("Signal acquired.")
+        # self.inst.write("WAVEFORM:PREAMBLE OFF")
+        # self.query('*OPC?')
 
-        self.set_channels()
-
-        self.inst.write("WAVEFORM:PREAMBLE OFF")
         for i, ch in enumerate(self.CHAN):  # for each channel
-            # Specify waveform source
-            self.inst.write(':WAVEFORM:SOURCE CHAN' + str(i))
-            # Specify waveform data format - most sig. byte transferred first
-            self.inst.write(":WAVEFORM:FORMAT ASCII")  # charged from SRIbinary, alternative RPB
-            # Number of bits per waveform point
-            # self.inst.write('WFMOutPre:BIT_Nr '+ str(self.BIT_NR))
-            # Number of bytes per data point
-            # self.inst.write(':Data:Width '+ str(self.BYTE_NR))
-            # Specify that we want to transfer N_SAMP points
-            self.inst.write(':WAVEFORM:POINTS ' + str(self.N_SAMP))
+            if ch:
+                print("channel: ",i+1)
+                # Specify waveform source
+                self.inst.write(':WAVEFORM:SOURCE CHAN' + str(i+1))
+                # Specify that we want to transfer N_SAMP points
+                if not n_samples:
+                    samples = self.N_SAMP
+                else:
+                    samples = n_samples
+                # self.inst.write(':WAVEFORM:POINTS '+str(samples))
+                self.inst.write(':WAVEFORM:POINTS MAXIMUM')
+                # Specify waveform data format - most sig. byte transferred first
+                self.inst.write(":WAVEFORM:FORMAT ASCII")  # charged from SRIbinary, alternative RPB
+                # Number of bits per waveform point
 
-            # Get scale and offset
-            # todo fix below
-            # verticalScale  = self.get_vertical_scale(i)
-
-            # yOffset = float(self.inst.query( 'WFMOutpre:YOFF?'))
-            # yzero = float(self.inst.query('WFMPRE:YZERO?'))
-
-            # Get the sample interval in seconds
-            Ts = float(self.inst.query('WAVEFORM:XINC?'))
-
-            #
-            # # Request the waveform data
-            # ADC_wave = self.inst.query_ascii_values(':WAVEFORM:DATA?', container= np.array)
-            ADC_wave_raw = self.inst.query(':WAVEFORM:DATA?')
-            n_leading = int(ADC_wave_raw[1])
-            ADC_wave = map(np.double,ADC_wave_raw[2+n_leading:])
-            # # Read in the data from the buffer, and scale
-            self.wave[ch] = ADC_wave
+                # self.inst.write('WFMOutPre:BIT_Nr '+ str(self.BIT_NR))
+                # Number of bytes per data point
+                # self.inst.write(':Data:Width '+ str(self.BYTE_NR))
 
 
-        # Get time series
-        self.wave['t'] = Ts*np.arange(1, self.N_SAMP)
-        return ADC_wave
+                # Get scale and offset
+                # verticalScale  = self.get_vertical_scale(i)
+
+                # yOffset = float(self.query( 'WFMOutpre:YOFF?'))
+                # yzero = float(self.query('WFMPRE:YZERO?'))
+
+                # Get the sample interval in seconds
+
+                #
+                # # Request the waveform data
+                # ADC_wave = self.query_ascii_values(':WAVEFORM:DATA?', container= np.array)
+                preable = self.query(':WAVeform:PREAMBLE?')
+                try:
+                    ADC_wave_raw = self.query(':WAV:DATA?')
+                except pyvisa.VisaIOError:
+                    print("Failed to read data, retrying...")
+                    ADC_wave_raw = self.query(':WAV:DATA?')
+                print("Data read successfully")
+
+                preable = list(map(float,preable.split(',')))
+                Ts = preable[4]
+                n_leading = int(ADC_wave_raw.split("#")[1][0])
+                ADC_wave = list(map(np.double, ADC_wave_raw.split("#")[1][n_leading+1:].split(',')))
+
+                # # Read in the data from the buffer, and scale
+                self.wave['CH' + str(i + 1)] = {}
+                self.wave['CH'+str(i+1)]["Amp"] = ADC_wave
+                self.wave['CH'+str(i+1)]["Time"] = Ts*np.arange(1, len(ADC_wave)+1)
+                # time.sleep(0.5)
+
+        return self.wave
+
+    def set_samplerate(self,samplerate):
+        """
+        Not possible to set samplerate on keysight scopes
+        :return:
+        """
+        pass
+
+    def query(self, string):
+        N_retry = 3
+        retry_counter=0
+        resp=None
+        while retry_counter < N_retry:
+            try:
+                resp = self.inst.query(string)
+            except pyvisa.VisaIOError:
+                retry_counter=+1
+                print("Query failed retrying...")
+            if resp:
+                return resp
+        raise pyvisa.VisaIOError
+
 
 
 class DSOX2024A(KeysightScope):
